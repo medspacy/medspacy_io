02f30220495342a81d0158c47d4591eca5423c89
Hannah Eyre <10606045+turbosheep@users.noreply.github.com>
2022-08-05 15:21:28 -0600

    partially added relationship functionality to ehost reader. ehost reader is still incomplete and is missing some ehost functionality.

--- medspacy_io/reader/base_reader.py
+++ medspacy_io/reader/base_reader.py
@@ -13,9 +13,15 @@ class BaseDocReader(object):
     A base class for document reader, define interfaces for subclasses to inherent from
     """
 
-    def __init__(self, nlp: Language = None, support_overlap: bool = False,
-                 log_level: int = logging.WARNING, encoding: str = None, doc_name_depth: int = 0,
-                 **kwargs):
+    def __init__(
+        self,
+        nlp: Language = None,
+        support_overlap: bool = False,
+        log_level: int = logging.WARNING,
+        encoding: str = None,
+        doc_name_depth: int = 0,
+        **kwargs
+    ):
         """
 
         @param nlp: Spacy Language model
@@ -39,16 +45,19 @@ class BaseDocReader(object):
         self.doc_name_depth = doc_name_depth
         self.support_overlap = support_overlap
         self.set_logger(log_level)
-        if not Doc.has_extension('doc_name'):
-            Doc.set_extension('doc_name', default='')
+        if not Span.has_extension("annotation_id"):
+            Span.set_extension("annotation_id", default="")
+        if not Doc.has_extension("relations"):
+            Doc.set_extension("relations", default=[])
+        if not Doc.has_extension("doc_name"):
+            Doc.set_extension("doc_name", default="")
         pass
 
     def set_logger(self, level: int = logging.DEBUG):
         self.logger = logging.getLogger(__name__)
         self.logger = logging.getLogger()
         handler = logging.StreamHandler()
-        formatter = logging.Formatter(
-            '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+        formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
         self.logger.setLevel(level)
@@ -129,8 +138,7 @@ class BaseDocReader(object):
         if doc_name_depth == -1:
             base_name = str(txt_file_path.absolute())
         elif doc_name_depth > 0:
-            base_name = str(txt_file_path.absolute() \
-                            .relative_to(txt_file_path.absolute().parents[doc_name_depth]))
+            base_name = str(txt_file_path.absolute().relative_to(txt_file_path.absolute().parents[doc_name_depth]))
         return base_name
 
     def process(self, doc: Doc, anno: str) -> Doc:
@@ -146,8 +154,9 @@ class BaseDocReader(object):
         else:
             return self.process_without_overlaps(doc, sorted_span, classes, attributes, relations)
 
-    def parse_to_dicts(self, anno: str, sort_spans: bool = False) -> Tuple[
-        _OrderedDictItemsView, OrderedDict, OrderedDict, OrderedDict]:
+    def parse_to_dicts(
+        self, anno: str, sort_spans: bool = False
+    ) -> Tuple[_OrderedDictItemsView, OrderedDict, OrderedDict, OrderedDict]:
         """
         Parse annotations into a Tuple of OrderedDicts, must be implemented in subclasses
         @param anno: The annotation string (can be a file path or file content, depends on how get_anno_content is implemented)
@@ -158,11 +167,21 @@ class BaseDocReader(object):
              attributes: a OrderedDict to map a attribute id to (attribute_name, attribute_value)
              relations: a OrderedDict to map a relation_id to (label, (relation_component_ids))
         """
-        return (None, None, None, None,)
+        return (
+            None,
+            None,
+            None,
+            None,
+        )
 
-    def process_without_overlaps(self, doc: Doc, sorted_spans: _OrderedDictItemsView, classes: OrderedDict,
-                                 attributes: OrderedDict,
-                                 relations: OrderedDict) -> Doc:
+    def process_without_overlaps(
+        self,
+        doc: Doc,
+        sorted_spans: _OrderedDictItemsView,
+        classes: OrderedDict,
+        attributes: OrderedDict,
+        relations: OrderedDict,
+    ) -> Doc:
         """:arg a SpaCy Doc, can be overwriten by the subclass as needed.
             This function will add spans to doc.ents (defined by SpaCy as default)
             which doesn't allow overlapped annotations.
@@ -207,9 +226,11 @@ class BaseDocReader(object):
             if token_start < 0 or token_start >= token_right_bound or token_end < 0 or token_end > token_right_bound:
                 raise ValueError(
                     "It is likely your annotations overlapped, which process_without_overlaps doesn't support parsing "
-                    "those. You will need to initiate the EhostDocReader with 'support_overlap=True' in the arguements")
+                    "those. You will need to initiate the EhostDocReader with 'support_overlap=True' in the arguements"
+                )
             if token_start >= 0 and token_end > 0:
                 span = Span(doc, token_start, token_end, label=classes[id][0])
+                span._.annotation_id = id
                 for attr_id in classes[id][1]:
                     if attr_id not in attributes:
                         continue
@@ -222,15 +243,35 @@ class BaseDocReader(object):
                 token_start = token_end
             else:
                 raise OverflowError(
-                    'The span of the annotation: {}[{}:{}] is out of document boundary.'.format(classes[id][0], start,
-                                                                                                end))
-            pass
+                    "The span of the annotation: {}[{}:{}] is out of document boundary.".format(classes[id][0], start, end)
+                )
         doc.ents = existing_entities + new_entities
+        rels = []
+        # print(relations)
+        # print(classes)
+        for rel_source, (rel_name, rel_target) in relations.items():
+            source_span = None
+            target_span = None
+            for ent in doc.ents:
+                for cls in classes[ent._.annotation_id][1]:
+                    if cls == rel_source:
+                        source_span = ent
+                        break
+                if ent._.annotation_id == rel_target:
+                    target_span = ent
+            rels.append((source_span, target_span, rel_name))
+        doc._.relations = rels
+
         return doc
 
-    def process_support_overlaps(self, doc: Doc, sorted_spans: _OrderedDictItemsView, classes: OrderedDict,
-                                 attributes: OrderedDict,
-                                 relations: OrderedDict) -> Doc:
+    def process_support_overlaps(
+        self,
+        doc: Doc,
+        sorted_spans: _OrderedDictItemsView,
+        classes: OrderedDict,
+        attributes: OrderedDict,
+        relations: OrderedDict,
+    ) -> Doc:
         """:arg a SpaCy Doc, can be overwriten by the subclass as needed.
             This function will add spans to doc._.concepts (defined in 'read' function above,
             which allows overlapped annotations.
@@ -275,31 +316,41 @@ class BaseDocReader(object):
                 #     self.logger.debug('\tfind token_start={}[{}]'.format(token_start, doc[token_start].idx))
                 #
                 # else:
-                self.logger.debug("To find {} between token_start ({}[{}]) and  token_right_bound({}[{}])"
-                                  .format(start, token_start, doc[token_start].idx,
-                                          token_right_bound, doc[token_right_bound].idx), )
+                self.logger.debug(
+                    "To find {} between token_start ({}[{}]) and  token_right_bound({}[{}])".format(
+                        start, token_start, doc[token_start].idx, token_right_bound, doc[token_right_bound].idx
+                    ),
+                )
                 token_start = self.find_start_token(start, token_start, token_right_bound, doc)
                 self.logger.debug("\tfind start token {}('{}')".format(token_start, doc[token_start]))
                 if end >= doc[-1].idx + doc[-1].__len__():
-                    self.logger.debug("end  ({}) >= doc[-1].idx ({}) + doc[-1].__len__() ({})".format(end, doc[-1].idx,
-                                                                                                      doc[
-                                                                                                          -1].__len__()))
+                    self.logger.debug(
+                        "end  ({}) >= doc[-1].idx ({}) + doc[-1].__len__() ({})".format(end, doc[-1].idx, doc[-1].__len__())
+                    )
                     token_end = token_right_bound + 1
                 else:
                     self.logger.debug(
-                        "To find token_end starts from {} between token_start ({}[{}]) and  token_right_bound({}[{}])"
-                            .format(end, token_start, doc[token_start].idx,
-                                    token_right_bound, doc[token_right_bound].idx))
+                        "To find token_end starts from {} between token_start ({}[{}]) and  token_right_bound({}[{}])".format(
+                            end, token_start, doc[token_start].idx, token_right_bound, doc[token_right_bound].idx
+                        )
+                    )
                     token_end = self.find_end_token(end, token_start, token_right_bound, doc)
                     self.logger.debug("\tFind end token {}('{}')".format(token_end, doc[token_end]))
             if token_start >= 0 and token_end > 0:
                 span = Span(doc, token_start, token_end, label=classes[id][0])
                 if self.logger.isEnabledFor(logging.DEBUG):
                     import re
-                    if re.sub('\s+', ' ', span._.span_txt) != re.sub('\s+', ' ', str(span)):
-                        self.logger.debug('{}[{}:{}]\n\t{}<>\n\t{}<>'.format(classes[id][0], token_start, token_end,
-                                                                             re.sub('\s+', ' ', span._.span_txt),
-                                                                             re.sub('\s+', ' ', str(span))))
+
+                    if re.sub("\s+", " ", span._.span_txt) != re.sub("\s+", " ", str(span)):
+                        self.logger.debug(
+                            "{}[{}:{}]\n\t{}<>\n\t{}<>".format(
+                                classes[id][0],
+                                token_start,
+                                token_end,
+                                re.sub("\s+", " ", span._.span_txt),
+                                re.sub("\s+", " ", str(span)),
+                            )
+                        )
                 for attr_id in classes[id][1]:
                     if attr_id not in attributes:
                         continue
@@ -316,8 +367,8 @@ class BaseDocReader(object):
 
             else:
                 raise OverflowError(
-                    'The span of the annotation: {}[{}:{}] is out of document boundary.'.format(classes[id][0], start,
-                                                                                                end))
+                    "The span of the annotation: {}[{}:{}] is out of document boundary.".format(classes[id][0], start, end)
+                )
             pass
         return doc
 
@@ -328,15 +379,14 @@ class BaseDocReader(object):
         @return: a string of text file content and corresponding annotation file content (for xml file, return annotation file path instead)
         """
         txt_file = self.check_file_validity(txt_file)
-        txt = ''
-        anno = ''
+        txt = ""
+        anno = ""
         if txt_file is not None:
             txt = self.get_txt_content(txt_file)
             anno = self.get_anno_content(txt_file)
         return txt, anno
 
-    def find_start_token(self, start: int, token_left_bound: int,
-                         token_right_bound: int, doc: Doc) -> int:
+    def find_start_token(self, start: int, token_left_bound: int, token_right_bound: int, doc: Doc) -> int:
         """
         Use binary search to find the token-based offset of an annotation (Span) start
 
@@ -348,20 +398,27 @@ class BaseDocReader(object):
         if token_right_bound > token_left_bound:
             mid = int(token_left_bound + (token_right_bound - token_left_bound) / 2)
             self.logger.debug(
-                "Find abs offset {} between {}[{}:{}] and {}[{}:{}], mid={}"
-                    .format(start, token_left_bound, doc[token_left_bound].idx,
-                            doc[token_left_bound].idx + doc[token_left_bound].__len__(),
-                            token_right_bound, doc[token_right_bound].idx,
-                            doc[token_right_bound].idx + doc[token_right_bound].__len__(), mid))
+                "Find abs offset {} between {}[{}:{}] and {}[{}:{}], mid={}".format(
+                    start,
+                    token_left_bound,
+                    doc[token_left_bound].idx,
+                    doc[token_left_bound].idx + doc[token_left_bound].__len__(),
+                    token_right_bound,
+                    doc[token_right_bound].idx,
+                    doc[token_right_bound].idx + doc[token_right_bound].__len__(),
+                    mid,
+                )
+            )
             if doc[mid].idx <= start < doc[mid].idx + len(doc[mid]):
                 self.logger.debug(
-                    "return mid={} when doc[{}]({}) < {} < doc[{}].idx+len(doc[{}])({})"
-                        .format(mid, mid, doc[mid].idx, start, mid, mid, doc[mid].idx + len(doc[mid])))
+                    "return mid={} when doc[{}]({}) < {} < doc[{}].idx+len(doc[{}])({})".format(
+                        mid, mid, doc[mid].idx, start, mid, mid, doc[mid].idx + len(doc[mid])
+                    )
+                )
                 return mid
             elif mid > 1 and doc[mid].idx > start >= doc[mid - 1].idx + len(doc[mid - 1]):
                 # sometime, manually created annotation can start outside SpaCy tokens, so adjustment is needed here.
-                self.logger.debug(
-                    "return mid={} when start {} between token {} and token []".format(mid, start, mid, mid - 1))
+                self.logger.debug("return mid={} when start {} between token {} and token []".format(mid, start, mid, mid - 1))
                 return mid
             elif doc[mid].idx > start:
                 if mid > 0:
@@ -372,7 +429,7 @@ class BaseDocReader(object):
                 if mid < len(doc) - 1:
                     return self.find_start_token(start, mid + 1, token_right_bound, doc)
                 else:
-                    return -1;
+                    return -1
 
         elif token_right_bound == token_left_bound:
             return token_left_bound
@@ -398,9 +455,9 @@ class BaseDirReader:
     A base class for directory reader, define interfaces for subclasses to inherent from
     """
 
-    def __init__(self, txt_extension: str = 'txt', recursive: bool = False,
-                 nlp: Language = None,
-                 docReaderClass: Type = None, **kwargs):
+    def __init__(
+        self, txt_extension: str = "txt", recursive: bool = False, nlp: Language = None, docReaderClass: Type = None, **kwargs
+    ):
         """
         @param txt_extension: the text file extension name (default is 'txt').
         @param recursive: whether read file recursively down to the subdirectories.
@@ -456,13 +513,13 @@ class BaseDirReader:
                 doc = self.reader.read(txt_file)
                 docs.append(doc)
             except:
-                raise IOError('An error occured while parsing annotation for document: {}'.format(txt_file.absolute()))
+                raise IOError("An error occured while parsing annotation for document: {}".format(txt_file.absolute()))
             pass
         return docs
 
     def list_files(self, input_dir_path: Path, file_extension: str, recursive: bool = False) -> List[Path]:
         if recursive:
-            files = list(input_dir_path.rglob('*.' + file_extension))
+            files = list(input_dir_path.rglob("*." + file_extension))
         else:
-            files = list(input_dir_path.glob('*.' + file_extension))
+            files = list(input_dir_path.glob("*." + file_extension))
         return files
--- medspacy_io/reader/ehost_reader.py
+++ medspacy_io/reader/ehost_reader.py
@@ -8,17 +8,25 @@ from lxml.etree import Element, iterparse
 from spacy.language import Language
 from spacy.tokens.span import Span
 
-from .base_reader import BaseDocReader, BaseDirReader
+from base_reader import BaseDocReader, BaseDirReader
 
 
 class EhostDocReader(BaseDocReader):
     """ This is a subclass of BaseDocReader to read in eHOST format files and generate SpaCy Docs
     """
 
-    def __init__(self, nlp: Language = None, support_overlap: bool = False,
-                 log_level: int = logging.WARNING, encoding: str = None, doc_name_depth: int = 0,
-                 schema_file: Union[str, Path] = '', store_anno_string: bool = False,
-                 use_adjudication: bool = False, **kwargs):
+    def __init__(
+        self,
+        nlp: Language = None,
+        support_overlap: bool = False,
+        log_level: int = logging.WARNING,
+        encoding: str = None,
+        doc_name_depth: int = 0,
+        schema_file: Union[str, Path] = "",
+        store_anno_string: bool = False,
+        use_adjudication: bool = False,
+        **kwargs
+    ):
         """
 
         @param nlp: a SpaCy language model
@@ -43,13 +51,20 @@ class EhostDocReader(BaseDocReader):
         if store_anno_string:
             if not Span.has_extension("span_txt"):
                 Span.set_extension("span_txt", default="")
-        super().__init__(nlp=nlp, support_overlap=support_overlap,
-                         log_level=log_level, encoding=encoding, doc_name_depth=doc_name_depth,
-                         schema_file=schema_file, store_anno_string=store_anno_string,
-                         use_adjudication=use_adjudication, **kwargs)
+        super().__init__(
+            nlp=nlp,
+            support_overlap=support_overlap,
+            log_level=log_level,
+            encoding=encoding,
+            doc_name_depth=doc_name_depth,
+            schema_file=schema_file,
+            store_anno_string=store_anno_string,
+            use_adjudication=use_adjudication,
+            **kwargs
+        )
         pass
 
-    def set_attributes(self, schema_file: Union[str, Path] = '', encoding: str = None) -> Set:
+    def set_attributes(self, schema_file: Union[str, Path] = "", encoding: str = None) -> Set:
         """
 
 
@@ -61,10 +76,11 @@ class EhostDocReader(BaseDocReader):
         """
         schema_file = self.check_file_validity(schema_file, False)
         attr_names = set()
+
         if schema_file is not None:
             root = etree.parse(str(schema_file.absolute()))
             for attr_def in root.iter("attributeDef"):
-                name = attr_def[0].text.replace(' ', '_')
+                name = attr_def[0].text.replace(" ", "_")
                 default_value = attr_def[2].text
                 if name not in attr_names and not Span.has_extension(name):
                     Span.set_extension(name, default=default_value)
@@ -91,11 +107,11 @@ class EhostDocReader(BaseDocReader):
         @return: the Path of the corresponding annotation file
         """
         txt_file_name = txt_file.name
-        anno_file_name = txt_file_name + '.knowtator.xml'
+        anno_file_name = txt_file_name + ".knowtator.xml"
         if not self.use_adjudication:
-            anno_file = Path(txt_file.parent.parent, 'saved', anno_file_name)
+            anno_file = Path(txt_file.parent.parent, "saved", anno_file_name)
         else:
-            anno_file = Path(txt_file.parent.parent, 'adjudication', anno_file_name)
+            anno_file = Path(txt_file.parent.parent, "adjudication", anno_file_name)
         self.check_file_validity(anno_file)
         return anno_file
 
@@ -110,23 +126,23 @@ class EhostDocReader(BaseDocReader):
              attributes: a OrderedDict to map a attribute id to (attribute_name, attribute_value)
              relations: a OrderedDict to map a relation_id to (label, (relation_component_ids))
         """
-        iter = etree.iterparse(xml_file, events=('start',))
+        iter = etree.iterparse(xml_file, events=("start",))
         # this doesn't seem elegant, but is said to be the fastest way
         spans = OrderedDict()
         classes = dict()
         attributes = OrderedDict()
         relations = OrderedDict()
         for event, ele in iter:
-            if ele.tag == 'annotation':
+            if ele.tag == "annotation":
                 id, start, end, span_text = self.parse_annotation_tag(ele, iter)
                 if self.store_anno_string:
                     spans[id] = (start, end, span_text)
                 else:
                     spans[id] = (start, end)
-            elif ele.tag == 'stringSlotMention':
+            elif ele.tag == "stringSlotMention":
                 attr_id, attr, value = self.parse_attribute_tag(ele, iter)
                 attributes[attr_id] = (attr, value)
-            elif ele.tag == 'classMention':
+            elif ele.tag == "classMention":
                 # <classMention id="EHOST_Instance_1">
                 #   <hasSlotMention id="EHOST_Instance_8"/>
                 #   <mentionClass id="Purulent">pain</mentionClass>
@@ -138,17 +154,20 @@ class EhostDocReader(BaseDocReader):
                 #     <hasSlotMention id="EHOST_Instance_68" />
                 #     <mentionClass id="Exclusions">presented</mentionClass>
                 # </classMention>
-                id = ele.get('id')
-            elif ele.tag == 'mentionClass':
-                class_tag = ele.get('id')
+                id = ele.get("id")
+            elif ele.tag == "mentionClass":
+                class_tag = ele.get("id")
                 if id not in classes:
-                    classes[id] = ['', []]
+                    classes[id] = ["", []]
                 classes[id][0] = class_tag
-            elif ele.tag == 'hasSlotMention':
+            elif ele.tag == "hasSlotMention":
                 if id not in classes:
-                    classes[id] = ['', []]
-                classes[id][1].append(ele.get('id'))
-            elif ele.tag == 'complexSlotMention':
+                    classes[id] = ["", []]
+                classes[id][1].append(ele.get("id"))
+                # for looking up relation source
+                if ele.get("id") in classes:
+                    classes[ele.get("id")][1].append(id)
+            elif ele.tag == "complexSlotMention":
                 # <complexSlotMention id="EHOST_Instance_41">
                 #     <mentionSlot id="Rel_A" />
                 #     <complexSlotMentionValue value="EHOST_Instance_29" />
@@ -156,6 +175,9 @@ class EhostDocReader(BaseDocReader):
                 rel_id, label, components = self.parse_relation_tag(ele, iter)
                 if rel_id is not None:
                     relations[rel_id] = (label, components)
+                    # for looking up the source
+                    classes[rel_id] = [label, []]
+
         if sort_spans:
             spans = sorted(spans.items(), key=lambda x: x[1][0])
         return spans, classes, attributes, relations
@@ -180,12 +202,12 @@ class EhostDocReader(BaseDocReader):
         span_text = None
         for i in range(0, 4):
             eve, child = iter.__next__()
-            if child.tag == 'mention':
-                id = child.get('id')
-            elif child.tag == 'span':
-                start = int(child.get('start'))
-                end = int(child.get('end'))
-            elif child.tag == 'spannedText':
+            if child.tag == "mention":
+                id = child.get("id")
+            elif child.tag == "span":
+                start = int(child.get("start"))
+                end = int(child.get("end"))
+            elif child.tag == "spannedText":
                 span_text = child.text
         return id, start, end, span_text
 
@@ -200,17 +222,21 @@ class EhostDocReader(BaseDocReader):
         @param iter: lxml element iterator
         @return: a Tuple of (attribute_id, corresponding entity id, attribute_name, attribute_value)
         """
-        id = ele.get('id')
-        attr = ''
-        value = ''
+        source = ele.get("id")
+        attr = ""
+        value = ""
         for i in range(0, 2):
             eve, child = iter.__next__()
-            if child.tag == 'mentionSlot':
-                attr = child.get('id')
+            if child.tag == "mentionSlot":
+                attr = child.get("id")
             else:
-                value = child.get('value')
-        return id, attr, value
+                value = child.get("value")
+        return source, attr, value
 
+    # <complexSlotMention id="EHOST_Instance_7">
+    #   <mentionSlot id="PROCEDURE_LOCATION" />
+    #   <complexSlotMentionValue value="EHOST_Instance_1" />
+    # </complexSlotMention>
     def parse_relation_tag(self, ele: Element, iter: iterparse) -> Tuple:
         """
         # TODO
@@ -218,12 +244,20 @@ class EhostDocReader(BaseDocReader):
         @param iter: lxml element iterator
         @return: a Tuple of (relation_id, (components ids contained in this relationship))
         """
-        return (None, None, None)
+        source = ele.get("id")
+        target = ""
+        name = ""
+        for i in range(0, 2):
+            eve, child = iter.__next__()
+            if child.tag == "mentionSlot":
+                name = child.get("id")
+            elif child.tag == "complexSlotMentionValue":
+                target = child.get("value")
+        return (source, name, target)
 
 
 class EhostDirReader(BaseDirReader):
-    def __init__(self, txt_extension: str = 'txt', recursive: bool = False,
-                 nlp: Language = None, **kwargs):
+    def __init__(self, txt_extension: str = "txt", recursive: bool = False, nlp: Language = None, **kwargs):
         """
 
         @param txt_extension: the text file extension name (default is 'txt').
@@ -231,6 +265,5 @@ class EhostDirReader(BaseDirReader):
         @param nlp: a SpaCy language model
         @param kwargs:other parameters to initiate EhostDocReader
         """
-        super().__init__(txt_extension=txt_extension, recursive=recursive, nlp=nlp,
-                         docReaderClass=EhostDocReader, **kwargs)
+        super().__init__(txt_extension=txt_extension, recursive=recursive, nlp=nlp, docReaderClass=EhostDocReader, **kwargs)
         pass
